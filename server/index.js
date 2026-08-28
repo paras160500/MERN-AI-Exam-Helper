407b8efe8c0d2d10860f882bba5596b324fe1d52
Paras Patel <38793371+paras160500@users.noreply.github.com>
2026-08-28 11:11:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const app = express();
 // Adding middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-ai-exam-helper-client.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
